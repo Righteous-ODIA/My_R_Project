9baad876aa06ee6ed86f3bad3c2a2fea0c442d96
Righteous-ODIA <co.rarebird@gmail.com>
2026-07-15 09:53:45 +0100

    Add R script after rebase

---
+++ loading_file_on_R.R
@@ -0,0 +1,14 @@
+my_data <- read.csv(file.choose())
+View(my_data)
+
+my_data1 <- read.csv(file.choose())
+View(my_data1)
+
+my_data2 <- read.csv(file.choose())
+View(my_data2)
+
+my_data3 <- read.csv(file.choose())
+View(my_data3)
+
+my_data4 <- read.csv(file.choose())
+View(my_data4)
